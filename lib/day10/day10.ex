9be851256c441ce5fb3e65745f2559a27f4e765e
Anders Urrang <aurrang@gmail.com>
2024-12-10 12:44:03 +0100

    day10 cleanup

--- lib/day10/day10.ex
+++ lib/day10/day10.ex
@@ -20,42 +20,23 @@ defmodule Day10.Day10 do
     {map, trailheads} = parse_file()
 
     trailheads
-    |> Enum.map(&count_peaks(map, MapSet.new(), [&1]))
+    |> Enum.map(&walk(map, [&1]))
+    |> Enum.map(&count_unique/1)
     |> Enum.sum()
   end
 
   def part_2() do
     {map, trailheads} = parse_file()
-
-    trailheads
-    |> Enum.map(&count_paths(map, [&1]))
-    |> Enum.sum()
+    Enum.count(walk(map, trailheads))
   end
 
-  defp count_peaks(_, _, []), do: 0
+  defp count_unique(list), do: MapSet.size(MapSet.new(list))
 
-  defp count_peaks(map, visited, [{pos, 9} | rest]),
-    do: 1 + count_peaks(map, MapSet.put(visited, pos), rest)
+  defp walk(_, []), do: []
+  defp walk(map, [{pos, 9} | rest]), do: [pos | walk(map, rest)]
+  defp walk(map, [{{x, y}, height} | rest]), do: walk(map, next_steps(map, x, y, height) ++ rest)
 
-  defp count_peaks(map, visited, [{{x, y}, height} | rest]) do
-    visited = MapSet.put(visited, {x, y})
-
-    next_steps =
-      get_next_steps(map, x, y, height)
-      |> Enum.reject(fn {pos, _} -> MapSet.member?(visited, pos) end)
-
-    count_peaks(map, visited, next_steps ++ rest)
-  end
-
-  defp count_paths(_, []), do: 0
-
-  defp count_paths(map, [{_, 9} | rest]), do: 1 + count_paths(map, rest)
-
-  defp count_paths(map, [{{x, y}, height} | rest]) do
-    count_paths(map, get_next_steps(map, x, y, height) ++ rest)
-  end
-
-  defp get_next_steps(map, x, y, height) do
+  defp next_steps(map, x, y, height) do
     [{x - 1, y}, {x + 1, y}, {x, y - 1}, {x, y + 1}]
     |> Enum.map(fn pos -> {pos, Map.get(map, pos)} end)
     |> Enum.filter(fn {_, new_height} -> new_height != nil && new_height - height == 1 end)
